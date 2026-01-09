745516ba182f55feae17451454256ce80daeb817
vaguettibfd <vaguetti.bfd@gmail.com>
2026-01-09 10:45:37 -0300

    ajustes iniciais 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Leandro Vaguetti', // Usually your GitHub org/user name.
+  projectName: 'JSOOEbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
